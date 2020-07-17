2d666f3ab7d680516ab7048676f1053da9cf1e02
Nurts <inurtas1999@gmail.com>
2020-07-17 20:48:52 +0600

    header change

--- packages/react-app/src/components/Header.js
+++ packages/react-app/src/components/Header.js
@@ -3,12 +3,10 @@ import { PageHeader } from 'antd';
 
 export default function Header(props) {
   return (
-    <div onClick={()=>{
-      window.open("https://github.com/austintgriffith/scaffold-eth");
-    }}>
+    <div>
       <PageHeader
-        title="📣 Attestor"
-        subTitle="a 🏗 Scaffold-ETH example app for IPFS"
+        title="Simple Store"
+        // subTitle="a 🏗 Scaffold-ETH example app for IPFS"
         style={{cursor:'pointer'}}
       />
     </div>
